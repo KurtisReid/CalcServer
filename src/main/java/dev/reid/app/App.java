7405cb0d9dccd1697fad720b42d4c403192ab944
Kurtis <kreid8@kent.edu>
2022-08-16 11:37:43 -0400

    Initial commit

---
+++ src/main/java/dev/reid/app/App.java
@@ -0,0 +1,13 @@
+package dev.reid.app;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create();
+
+        app.get("hello", ctx ->{
+            ctx.result("Hi everyone");
+        });
+    }
+}
